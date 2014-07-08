5a48aa898a9a20a409fd69c9cb9df0ba9e93fae4
承风 <libiao.lb@taobao.com>
2014-07-08 15:46:24 +0800

    js、文档、demo完成

--- 1.0/index.js
+++ 1.0/index.js
@@ -1,29 +1,132 @@
 /**
- * @fileoverview 
+ * @fileoverview 60fps目标的scroll辅助插件
  * @author 承风<libiao.lb@tmall.com>
  * @module hp-scroll
  **/
-KISSY.add(function (S, Node,Base) {
-    var EMPTY = '';
-    var $ = Node.all;
+
+KISSY.add('gallery/hp-scroll/1.0/index', function(S, DOM, Event) {
+    var cssSupport = true;
+    var inScroll = false;
+    var addedCls = false;
+    var HpScroll = {
+        init : function() {
+            //检测下CSS属性是否支持，支持范围见链接：http://caniuse.com/#search=pointer-events
+            cssSupport = checkCssSupport();
+
+            //劫持和修改默认的event.on方法
+            if(!cssSupport) {
+                this.delegateScroll();
+            }
+            var self = this;
+
+            S.ready(function() {
+                self.start();
+            });
+        },
+        /**
+         * 劫持整合页面的event
+         */
+        delegateScroll : function() {
+            var defaultOnFn = Event.on;
+            //管理mouseenter，mouseleave两个个响应
+            Event.on = function(targets, type, fn, context) {
+                var customFun = fn;
+                if (/[mouseenter | mouseleave]/.test(type)) {
+                    fn = function(e) {
+                        //如果在scroll进行中就啥都不干， 否则就触发其定义的内容
+                        if (inScroll) {
+                            return;
+                        } else {
+                            customFun(e);
+                        }
+                    }
+                }
+                defaultOnFn(targets, type, fn, context);
+            };
+        },
+        /**
+         * 开始运行
+         */
+        start : function() {
+            var timer;
+            Event.on(window, 'scroll touchmove', function() {
+                if(!inScroll) {
+                    inScroll = true;
+                    //如果支持禁掉，就禁掉it！
+                    if(!addedCls && cssSupport) {
+                        DOM.css('body', {
+                            'pointerEvents' : 'none'
+                        });
+                        addedCls = true;
+                    }
+                }
+
+                clearTimeout(timer);
+
+                timer = setTimeout(function(){
+                    inScroll = false;
+
+                    //清除css
+                    if(cssSupport) {
+                        DOM.css('body', {
+                            'pointerEvents' : ''
+                        });
+                        addedCls = false;
+                    }
+                }, 300);
+            });
+            //禁掉状态的click和tap都失效的哦~~so要自己代理下
+            Event.on('body', 'click tap', function(e) {
+                //scroll中的点击，清除计时
+                if(!e.synthetic && inScroll) {
+                    clearTimeout(timer);
+                    inScroll = false;
+
+                    if(cssSupport) {
+                        //清除css
+                        DOM.css('body', {
+                            'pointerEvents' : ''
+                        });
+                        addedCls = false;
+
+                        var pos = {
+                            x : event.clientX,
+                            y : event.clientY
+                        };
+                        //scroll中的
+                        dispatchEvt(pos, e.type);
+                    }
+                }
+            });
+        }
+    };
+
     /**
-     * 
-     * @class HpScroll
-     * @constructor
-     * @extends Base
+     * 检测下是否支持，不支持就拉倒
      */
-    function HpScroll(comConfig) {
-        var self = this;
-        //调用父类构造函数
-        HpScroll.superclass.constructor.call(self, comConfig);
+    function checkCssSupport() {
+        var el = DOM.create('<div></div>');
+        DOM.css(el, {
+            'pointerEvents' : 'auto'
+        });
+        return DOM.css(el, 'pointerEvents') === 'auto';
     }
-    S.extend(HpScroll, Base, /** @lends HpScroll.prototype*/{
 
-    }, {ATTRS : /** @lends HpScroll*/{
+    /**
+     * 代理click和tap
+     */
+    function dispatchEvt(pos, type) {
+        var el = document.elementFromPoint(pos.x, pos.y);
 
-    }});
+        Event.fire(el, type, {
+            synthetic : true
+        });
+    }
+
     return HpScroll;
-}, {requires:['node', 'base']});
+}, {
+    requires : ['dom', 'event']
+});
 
 
 
--- README.md
+++ README.md
@@ -12,13 +12,19 @@
 
 * [高级浏览器支持CSS `pointer-events:none`](http://caniuse.com/#search=pointer-events)来禁掉元素触发鼠标事件和样式。直接在scroll开始时候赋值到`<body>`的行内样式上，整个页面不再触发鼠标事件。
 
-* 其余浏览器，对KISSY的DOM内的on方法进行了劫持，当scroll时候不处罚`mouseenter`,`mouseleave`响应。
+* 其余浏览器，对KISSY的Event内的on方法进行了劫持，当scroll时候不处罚`mouseenter`,`mouseleave`响应。
 
 
 ## 使用方式
 
-* 在注册事件之前，引入本js，调用init方法。会自动管理scroll的效率。。。够简单了
+* **在注册事件之前**，先入本js，调用init方法。会自动管理scroll的效率。。。够简单了
+* why？保障低端浏览器下，Event.on方法被正确重写。
 
+## 缺陷
+
+* 目前只对KISSY的Event内的on方法做了劫持。对于低端浏览器而言，只有使用Event.on来绑定的mouse事件会被劫持而提高效率。
+* [高级浏览器](http://caniuse.com/#search=pointer-events)，当样式pointer-events绑定后，自然不会触发mouse事件，就不会有这个劫持逻辑了。
+* 源代码超简单，欢迎吐槽，讨论，优化
 
 ## changelog
 
